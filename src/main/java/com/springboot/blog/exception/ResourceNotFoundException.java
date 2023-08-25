c1976416252ba7a1c38c6f03db2633b6ba0d47f5
victor alvarez <victorfelipealvarezrojas@hotmail.com>
2023-08-25 00:08:17 -0400

    add dtopost

--- src/main/java/com/springboot/blog/exception/ResourceNotFoundException.java
+++ src/main/java/com/springboot/blog/exception/ResourceNotFoundException.java
@@ -1,30 +1,21 @@
 package com.springboot.blog.exception;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import lombok.Getter;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Getter
 @ResponseStatus(value = NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-    private String resourceName;
-    private String fieldName;
-    private Object fieldValue;
+    private final String resourceName;
+    private final String fieldName;
+    private final Object fieldValue;
 
     public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
         super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
-    }
-
-    public String getResourceName() {
-        return resourceName;
-    }
-
-    public String getFieldName() {
-        return fieldName;
-    }
-
-    public Object getFieldValue() {
-        return fieldValue;
     }
 }
